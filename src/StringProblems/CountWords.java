87683914b33163455bed8e4d083c4c7cb46d8959
thaniyelraja <rajaselvan2003@gmail.com>
2026-03-28 07:34:32 +0530

    feat(string): add basic string problem programs

---
+++ src/StringProblems/CountWords.java
@@ -0,0 +1,11 @@
+package StringProblems;
+
+public class CountWords {
+	public static void main(String[] args) {
+		String str = "Hello World";
+
+		String[] words = str.trim().split("\\s+");
+
+		System.out.println(words.length);
+	}
+}
