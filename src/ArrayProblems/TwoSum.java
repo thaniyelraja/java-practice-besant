54203143f3efa4de89629095ccb4f10bbf2448f2
thaniyelraja <rajaselvan2003@gmail.com>
2026-03-28 07:18:15 +0530

    feat(array): add basic array problem programs

---
+++ src/ArrayProblems/TwoSum.java
@@ -0,0 +1,19 @@
+package ArrayProblems;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 6, 5, 3, 7, 2 };
+		int target = 7;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == target) {
+					System.out.println("Addition of " + arr[i] + " and " + arr[j] + " is " + target);
+				}
+
+			}
+		}
+
+	}
+
+}
